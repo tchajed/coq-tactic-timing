74b25bcb99e70d4e3df60171d14f8f4c81d7d3cd
Tej Chajed <tchajed@gmail.com>
2024-08-02 09:32:42 -0500

    Format with black

--- tactic-timing.py
+++ tactic-timing.py
@@ -3,6 +3,7 @@
 import re
 import pandas as pd
 
+
 class TimeReport:
     def __init__(self, tactic, time, success, backtracks):
         self.tactic = tactic
@@ -12,11 +13,16 @@ class TimeReport:
 
     @classmethod
     def parse(cls, line):
-        m = re.match("Tactic call ((?P<tactic>.+) )?" +
-                "ran for (?P<time>[0-9.]+) secs.*\\(" +
-                ("(?P<result>(success)" + "|"
-                    "(failure( after (?P<backtracks>[0-9]+) backtracking)?))") +
-                "\\)(\\n)?", line)
+        m = re.match(
+            "Tactic call ((?P<tactic>.+) )?"
+            + "ran for (?P<time>[0-9.]+) secs.*\\("
+            + (
+                "(?P<result>(success)" + "|"
+                "(failure( after (?P<backtracks>[0-9]+) backtracking)?))"
+            )
+            + "\\)(\\n)?",
+            line,
+        )
         if m is None:
             return None
         tactic = m.group("tactic")
@@ -34,6 +40,7 @@ class TimeReport:
         return [self.tactic, self.time, self.success]
 
     df_header = ["tactic", "time", "success"]
+
 
 if __name__ == "__main__":
     import argparse
@@ -53,17 +60,15 @@ if __name__ == "__main__":
             """,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("-t", "--threshold",
-            type=float,
-            default=0.1,
-            help="threshold to print time")
-    parser.add_argument("--slow-log",
-            dest="slow_log",
-            type=bool,
-            help="print the slow log")
-    parser.add_argument("coq_output",
-            type=argparse.FileType(),
-            help="file with Coq's output")
+    parser.add_argument(
+        "-t", "--threshold", type=float, default=0.1, help="threshold to print time"
+    )
+    parser.add_argument(
+        "--slow-log", dest="slow_log", type=bool, help="print the slow log"
+    )
+    parser.add_argument(
+        "coq_output", type=argparse.FileType(), help="file with Coq's output"
+    )
 
     args = parser.parse_args()
     thresh = args.threshold
@@ -84,8 +89,8 @@ if __name__ == "__main__":
             print(slowlog)
         print()
 
-    tactics = df.groupby(['tactic', 'success'], sort=False)
+    tactics = df.groupby(["tactic", "success"], sort=False)
     print("Stats")
-    pd.set_option('display.width', 150)
-    aggregated = tactics['time'].agg(['sum', 'count', 'mean', 'max'])
-    print(aggregated.sort_values(by='sum', ascending=False))
+    pd.set_option("display.width", 150)
+    aggregated = tactics["time"].agg(["sum", "count", "mean", "max"])
+    print(aggregated.sort_values(by="sum", ascending=False))
